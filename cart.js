b235919573a7a42ebb13d7aecf90ae78c6004172
anushka bhandari <bhandarianushka670@gmail.com>
2023-05-22 16:04:24 +0530

    add to cart functionality added

---
+++ cart.js
@@ -0,0 +1,93 @@
+var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+
+    
+    var cartItemsContainer = document.getElementById('cart-items');
+    var buyerNameInput = document.getElementById('buyer-name');
+    var buyerEmailInput = document.getElementById('buyer-email');
+
+
+    cartItems.forEach(function(item) {
+      var itemElement = document.createElement('div');
+      itemElement.innerHTML = item.name + ' - $' + item.price + ' - Quantity: ' + item.quantity;
+
+      
+      var increaseQuantityButton = document.createElement('button');
+      increaseQuantityButton.innerText = '+';
+      increaseQuantityButton.onclick = function() {
+        item.quantity++;
+        updateCartDisplay();
+      };
+      itemElement.appendChild(increaseQuantityButton);
+
+      cartItemsContainer.appendChild(itemElement);
+    });
+    function updateCartDisplay() {
+        
+        cartItemsContainer.innerHTML = '';
+  
+        
+        cartItems.forEach(function(item) {
+          var itemElement = document.createElement('div');
+          itemElement.innerHTML = item.name + ' - $' + item.price + ' - Quantity: ' + item.quantity;
+  
+          
+          var increaseQuantityButton = document.createElement('button');
+          increaseQuantityButton.innerText = '+';
+          increaseQuantityButton.onclick = function() {
+            item.quantity++;
+            updateCartDisplay();
+          };
+          itemElement.appendChild(increaseQuantityButton);
+  
+          cartItemsContainer.appendChild(itemElement);
+        });
+  
+        
+        updateSubtotal();
+      }
+      function updateSubtotal() {
+        var subtotal = 0;
+  
+        
+        cartItems.forEach(function(item) {
+          subtotal += item.price * item.quantity;
+        });
+  
+        
+        var subtotalElement = document.getElementById('subtotal');
+        subtotalElement.innerHTML = 'Subtotal: $' + subtotal.toFixed(2);
+      }
+  
+      
+      function emptyCart() {
+        
+        localStorage.removeItem('cartItems');
+  
+    
+        cartItemsContainer.innerHTML = '';
+  
+
+        var subtotalElement = document.getElementById('subtotal');
+        subtotalElement.innerHTML = '';
+      }
+      function checkout() {
+
+        var order = {
+            buyerName: buyerNameInput.value,
+            buyerEmail: buyerEmailInput.value,
+            items: cartItems
+          };
+
+        localStorage.setItem('order', JSON.stringify(order));
+
+    
+         window.location.href = 'transaction.html';
+
+  
+        
+        emptyCart();
+  
+        
+    
+      }
+  
